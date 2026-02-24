3f88eb262277598eab000d99fd8e40eec9d604fb
Santiago <sahurtaro@gmail.com>
2026-02-24 11:43:56 -0500

    C2 - constructors

--- OOP.Course.cpp
+++ OOP.Course.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
-int main(){}
+class YoutubeChannel {
+public: 
+    string Name; string OwnerName; int SubscribersCount; list <string> PublishedVideoTitles;
+    YoutubeChannel(string name, string ownerName){
+        Name=name;
+        OwnerName=ownerName;
+        SubscribersCount=0;
+        
+    };
+    void GetInfo(){
+         cout << "Name: " <<Name<<endl;    
+    cout << "Owner name: "<<OwnerName<<endl;
+    cout << "Susbcribers count: "<<SubscribersCount<<endl;
+    cout << "Videos: "<<endl;
+    for(string VideoTitle:PublishedVideoTitles){
+        cout<<VideoTitle<<endl;
+    }
+    }
+};
+
+int main(){
+    YoutubeChannel ytChannel("PeluTV","Santiago H");
+    ytChannel.PublishedVideoTitles.push_back("Ivan el trolazo");
+    ytChannel.GetInfo();
+
+   
+
+}
